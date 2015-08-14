d51d2d3cf3f13fa8760561215dab569b1fa82dd8
Jean-Charles Sisk <jasisk@gmail.com>
2015-08-14 14:28:24 -0400

    change grabbing user from section to config

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func runSSH(c command, host string, section *SshConfigFileSection, agentForwardi
 	}
 	defer func() {
 		session.Close()
-		log.Printf("Session complete from %s@%s", section.User, host)
+		log.Printf("Session complete from %s@%s", config.User, host)
 	}()
 
 	for key, value := range c.Env {
