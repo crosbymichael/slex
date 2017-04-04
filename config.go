a34ebd15d77810ffe5010ec4bc4fe62a5046cf2c
Shawn Milochik <shawn@milochik.com>
2017-04-04 13:42:20 -0400

    fix ssh config parsing

--- config.go
+++ config.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/user"
@@ -19,34 +18,6 @@ type SSHConfigFileSection struct {
 	HostName     string
 	Port         string
 	IdentityFile string
-}
-
-// parseSSHConfigFileSection parses a section from the ~/.ssh/config file
-func parseSSHConfigFileSection(content string) *SSHConfigFileSection {
-	section := &SSHConfigFileSection{}
-
-	for n, line := range strings.Split(content, "\n") {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
-
-		if n == 0 {
-			section.Host = line
-		} else if strings.HasPrefix(line, "ForwardAgent") {
-			section.ForwardAgent = strings.TrimSpace(strings.TrimPrefix(line, "ForwardAgent"))
-		} else if strings.HasPrefix(line, "User") {
-			section.User = strings.TrimSpace(strings.TrimPrefix(line, "User"))
-		} else if strings.HasPrefix(line, "HostName") {
-			section.HostName = strings.TrimSpace(strings.TrimPrefix(line, "HostName"))
-		} else if strings.HasPrefix(line, "Port") {
-			section.Port = strings.TrimSpace(strings.TrimPrefix(line, "Port"))
-		} else if strings.HasPrefix(line, "IdentityFile") {
-			section.IdentityFile = strings.TrimSpace(strings.TrimPrefix(line, "IdentityFile"))
-		}
-	}
-	log.Debugf("parsed ssh config file section: %s", section.Host)
-	return section
 }
 
 // parseSSHConfigFile parses the ~/.ssh/config file and build a list of section
@@ -69,15 +40,34 @@ func parseSSHConfigFile() (map[string]*SSHConfigFileSection, error) {
 		}
 		return nil, err
 	}
-
-	for _, split := range strings.Split(string(content), "Host ") {
-		split = strings.TrimSpace(split)
-		if split == "" {
+	lines := strings.Split(string(content), "\n")
+	current := &SSHConfigFileSection{}
+	for _, line := range lines {
+		parts := strings.SplitN(strings.TrimSpace(line), " ", 2)
+		if len(parts) != 2 {
 			continue
 		}
+		key := strings.ToLower(strings.TrimSpace(parts[0]))
+		val := strings.TrimSpace(parts[1])
+		if key == "host" {
+			if current.Host != "" {
+				sections[current.Host] = current
+			}
+			current = &SSHConfigFileSection{Host: val}
+		} else if key == "hostname" {
+			current.HostName = val
+		} else if key == "user" {
+			current.User = val
+		} else if key == "port" {
+			current.Port = val
+		} else if key == "forwardagent" {
+			current.ForwardAgent = val
+		}
+	}
 
-		section := parseSSHConfigFileSection(split)
-		sections[section.Host] = section
+	// add last host to map
+	if current.Host != "" {
+		sections[current.Host] = current
 	}
 
 	return sections, nil
